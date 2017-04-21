21a0e01999429ed25360be0959ffc99e915a4de3
shsxssp72 <1260027595@qq.com>
2017-04-21 13:04:06 +0800

    ChainList,SequenceList,HashChain
    Initial version

---
+++ Constants.h
@@ -0,0 +1,17 @@
+//
+// Created by 星落_月残 on 2017/4/13.
+//
+
+#ifndef BASICDATASTRUCTUREANDALGORITHMS_CONSTANTS_H
+#define BASICDATASTRUCTUREANDALGORITHMS_CONSTANTS_H
+
+//链式表常量
+constexpr int MaxChainLength=100;
+//顺序表常量
+constexpr int MaxSequenceLength=100;
+constexpr int InitialSequenceSize=50;
+constexpr int NewDynamicArrayIncrement=10;
+//哈希表常量
+constexpr int HashSize=37;
+
+#endif //BASICDATASTRUCTUREANDALGORITHMS_CONSTANTS_H
